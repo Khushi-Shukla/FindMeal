eff58447985cfa77d74b90e00f3527155363569a
unknown <khushishukla0502@gmail.com>
2022-01-17 18:59:01 +0530

    API

--- FoodList/list.js
+++ FoodList/list.js
@@ -35,7 +35,7 @@ function getMealList(){
     let searchInputTxt = document.getElementById('search-input').value.trim();
     setTimeout(
         ()=>
-        fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInputTxt}`)
         .then(response => response.json())
         .then(data => {
             hideLoading()
